00ccb218a08ee42347b3617c2a30464a4891354f
yasszoug <ysnowtech@gmail.com>
2015-07-06 20:24:17 +0100

    Update config_clean.py

--- config_clean.py
+++ config_clean.py
@@ -1,12 +1,12 @@
 
 # twitter credentials from dev.twitter.com
-app_key=""
-app_secret=""
-oauth_token=""
-oauth_token_secret=""
+app_key="6tmsBsmBvUXKDSvoK2j4LPhc7"
+app_secret="rzqWI2vsT319P8Gte2AB0YbTeqIRQZo2u7j1kiteXKtpmHX9Mi"
+oauth_token="3362865713-ok11ghQVML0badoj9OkdisMPqhwYT9uW9ZrgM4L"
+oauth_token_secret="3zd8c005mdQBRTdI1AJyQxCBBON6draNUP9xKYicUfU8X"
 
 # the twitter screenname of the owner (for error messages etc)
-owner=""
+owner="Sport_Alert_01"
 
 # the module containing the fetch_data() function
 data_fetcher='bvb'
